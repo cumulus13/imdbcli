0859098d509e201cb1b746a3a3aa535c86e4c95d
cumulus13 <cumulus13@gmail.com>
2025-01-06 17:58:29 +0700

    version: 0.20 ~ 2025-01-06 17:58:28:415473

--- trailer.py
+++ trailer.py
@@ -41,10 +41,10 @@ def show(content):
     # Create and configure the browser window
     if is_html(content):
         # If content is HTML, create a window with HTML code
-        window = webview.create_window('Custom HTML Viewer', html=content, width=720, height=480, resizable=False)
+        window = webview.create_window('Custom HTML Viewer', html=content, width=720, height=480, resizable=False, on_top = True)
     else:
         # If content is a URL, create a window with the URL
-        window = webview.create_window('IMDb Trailer Browser', content, width=720, height=480, resizable=False)
+        window = webview.create_window('IMDb Trailer Browser', content, width=720, height=480, resizable=False, on_top = True)
     
     window.events.loaded += lambda: inject_js(window)  # Inject CSS when the page is loaded
 
